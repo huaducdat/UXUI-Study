db902847d2c97c607c5be88a5dc40bf0e4f16a50
huaducdat <diepy94@gmail.com>
2025-07-14 16:32:58 +0700

    upadte portfolio

---
+++ Portfolio/js/part1.js
@@ -0,0 +1,71 @@
+const yearShow = document.getElementById("year-show");
+let radios = document.querySelectorAll("input[name=step]");
+
+
+
+const slider = document.getElementById("scrollSlider");
+const scrollContainer = document.getElementById("slider-catalog");
+
+
+function setYearTxt() {
+    radios.forEach(radio => {
+        radio.addEventListener("change", () => {
+            if (radio.checked) {
+                yearShow.innerHTML = switchKey(radio.value);
+            }
+        })
+    })
+}
+function switchKey(value) {
+    switch (value) {
+        case "0": return "2013";
+        case "1": return "2015";
+        case "2": return "2017";
+        case "3": return "2018";
+        case "4": return "2025";
+        case "5": return "Continuing...";
+        default:
+            break;
+    }
+}
+setYearTxt();
+
+slider.addEventListener("input", () => {
+    const maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+    scrollContainer.scrollLeft = (slider.value / 100) * maxScroll;
+    // console.log("Max Scroll: " + maxScroll);
+});
+scrollContainer.addEventListener("scroll", () =>{
+    const maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+    const percent = (scrollContainer.scrollLeft/maxScroll) * 100;
+    slider.value = percent;
+})
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+scrollContainer.addEventListener("mousedown", (e) => {
+  isDown = true;
+  scrollContainer.classList.add("active"); // optional: style khi đang kéo
+  startX = e.pageX - scrollContainer.offsetLeft;
+  scrollLeft = scrollContainer.scrollLeft;
+});
+
+scrollContainer.addEventListener("mouseleave", () => {
+  isDown = false;
+  scrollContainer.classList.remove("active");
+});
+
+scrollContainer.addEventListener("mouseup", () => {
+  isDown = false;
+  scrollContainer.classList.remove("active");
+});
+
+scrollContainer.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - scrollContainer.offsetLeft;
+  const walk = (x - startX) * 1.2; // tốc độ kéo, điều chỉnh nếu muốn
+  scrollContainer.scrollLeft = scrollLeft - walk;
+});
