e08074326490a28bdb6ab7207aa0de375df42ff6
huaducdat <diepy94@gmail.com>
2025-07-14 13:36:41 +0700

    update1

--- Job11/asset/js/script2.js
+++ Job11/asset/js/script2.js
@@ -3,7 +3,20 @@ arr.push("An");
 arr.push("Binh");
 arr.push("Chi");
 console.log(arr);
-arr.splice(1,1);
-console.log(arr);
-arr.unshift("Dung");
-console.log(arr);
+
+
+const run = function () {
+    setTimeout(() => {
+        arr.unshift("Dung");
+        console.log(arr);
+    }, 1000);
+}
+
+function run1() {
+    setTimeout(() => {
+        arr.splice(1, 1);
+        console.log(arr);
+    }, 1000);
+    run();
+}
+run1();
